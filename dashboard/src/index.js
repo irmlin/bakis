ca27805239d67045e454d82ef2079d835a2a3e84
irmlin <irmantas.l75@gmail.com>
2024-03-24 19:27:53 +0200

    Update frontend core files

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -1,23 +1,33 @@
-import React from 'react';
-import ReactDOM from "react-dom";
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import App from './Pages/App';
-import reportWebVitals from './reportWebVitals';
+/**
+=========================================================
+* Material Dashboard 2 React - v2.2.0
+=========================================================
 
-ReactDOM.render(
-  <React.StrictMode>
-          <Router>
-            <Routes>
-              <Route path="/" element={<App/>}/>
-              {/* For invalid paths, redirect to /login */}
-              <Route path="/*" element={<App/>}/>
-            </Routes>
-          </Router>
-  </React.StrictMode>,
-  document.getElementById("root")
+* Product Page: https://www.creative-tim.com/product/material-dashboard-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+
+// Material Dashboard 2 React Context Provider
+import { MaterialUIControllerProvider } from "./Context";
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
+  <BrowserRouter>
+    <MaterialUIControllerProvider>
+      <App />
+    </MaterialUIControllerProvider>
+  </BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
