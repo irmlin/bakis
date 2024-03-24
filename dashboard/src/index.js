d44d678cdb0cbd71e6b45d7f3ad5c84428c226ed
irmlin <irmantas.l75@gmail.com>
2024-03-24 18:00:14 +0200

    Update project structure.

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import ReactDOM from "react-dom";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import App from './Pages/App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+          <Router>
+            <Routes>
+              <Route path="/" element={<App/>}/>
+              {/* For invalid paths, redirect to /login */}
+              <Route path="/*" element={<App/>}/>
+            </Routes>
+          </Router>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
